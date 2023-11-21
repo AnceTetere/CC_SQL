d71246f27ecb8bfbbf5be73cfb38329662bb9e62
AnceTetere <ance.tetere@outlook.com>
2023-11-21 19:26:46 +0200

    Initial queries and clean up.

---
+++ 3_select_all.sql
@@ -0,0 +1,7 @@
+SELECT date_received, [product_name], [sub_product], [issue], [sub_issue], [consumer_complaint_narrative], [company_public_response],
+[company], [state_name], [zip_code], [tags], [consumer_consent_provided], [submitted_via], [date_sent], [company_response_to_consumer], 
+[timely_response], [consumer_disputed], [complaint_id]
+FROM Consumer_complaints
+
+SELECT *
+FROM Consumer_complaints
---
+++ 4_productIssue.sql
@@ -0,0 +1,2 @@
+SELECT [product_name], [issue], [company]
+FROM Consumer_complaints
